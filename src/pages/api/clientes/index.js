0fbd783c5cb668b267e4d22113ef30404f07efed
Roger-Franco <rogerfrancogalt@gmail.com>
2021-12-19 20:05:49 -0400

    Usando API #01

---
+++ src/pages/api/clientes/index.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+  
+  if(req.method == 'GET') {
+    handleGet(req, res)
+  } else {
+    res.status(405).send()
+  }
+}
+
+function handleGet(req, res) {
+  res.status(200).send({
+    id: 3,
+    nome: 'Maria',
+    email: 'mariamariamaria@mariamail.com'
+  })
+}
